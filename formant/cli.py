f4812a430888e254ee2138d43cac562ac4609522
Philip Horger <campadrenalin@gmail.com>
2020-06-23 14:12:10 -0700

    CLI wrapper

---
+++ formant/cli.py
@@ -0,0 +1,75 @@
+import sys, os
+from formant.template import template
+
+def parse(argv):
+    positional = []
+    flag_args  = {}
+    current_flag = None
+
+    for arg in argv:
+        if current_flag:
+            flag_args[current_flag] = arg
+            current_flag = None
+            continue
+
+        if arg.startswith('--'):
+            arg, eq, value = arg.partition('=')
+            current_flag = arg.strip('-').replace('-', '_')
+            if eq:
+                flag_args[current_flag] = value
+                current_flag = None
+            continue
+
+        positional.append(arg)
+
+    assert current_flag is None, f'No value provided for {current_flag}'
+    return positional, flag_args
+
+def read_file(filename='-'):
+    if filename == '-':
+        return sys.stdin.read()
+    with open(filename, 'r') as f:
+        return f.read()
+
+def fmnt_print(*argv, **env):
+    '''
+    formant print [filename] --some-arg='Some value'
+    '''
+    positional, flag_args = parse(argv)
+    assert len(positional) <= 1, "Expect one positional argument (filename)"
+
+    text = read_file(*positional)
+    try:
+        print(template(text, flag_args, env))
+    except NameError as ne:
+        import re
+        name = re.fullmatch(r"name '(\w+)' is not defined", str(ne))
+        if name:
+            ne = name.group(1)
+        print(f'Missing parameter: {ne}', file=sys.stderr)
+
+def fmnt_help(subcommand):
+    '''
+    formant help [subcommand] :: This message
+    formant print             :: Print a template with parameter substitution
+    '''
+    print(subcommand.__doc__, file=sys.stderr)
+
+def run_cli(argv=sys.argv, env=os.environ):
+    subcommands = {
+        'print': fmnt_print,
+        'help':  fmnt_help,
+    }
+    def get_handler(argn):
+        if len(argv)-1 < argn:
+            return fmnt_help
+        return subcommands.get(argv[argn], fmnt_help)
+
+    handler = get_handler(1)
+    if handler is fmnt_help:
+        return fmnt_help(get_handler(2))
+    else:
+        return handler(*argv[2:], **env)
+
+if __name__ == '__main__':
+    run_cli()
---
+++ tests/test_cli.py
@@ -0,0 +1,41 @@
+import pytest
+from unittest.mock import patch
+from formant.cli import *
+
+@pytest.mark.parametrize('argv, pos, flags', [
+    ([], [], {}),
+    (['A positional arg'], ['A positional arg'], {}),
+    (['one', 'two', 'three'], ['one', 'two', 'three'], {}),
+    (['--flag', 'value'], [], {'flag': 'value'}),
+    (['--flag-with-hyphens', 'value'], [], {'flag_with_hyphens': 'value'}),
+    (['--flag=Some Text'], [], {'flag': 'Some Text'}),
+    (['--flag=Some=Text'], [], {'flag': 'Some=Text'}),
+    (['pos1', '--flag1=f1', 'pos2', '--flag2', 'f2'],
+        ['pos1', 'pos2'],
+        {'flag1':'f1', 'flag2':'f2'}),
+])
+def test_parse(argv, pos, flags):
+    assert parse(argv) == (pos, flags)
+
+files = {
+    '-': 'A simple STDIN example',
+    'hello.txt': 'Hello, <% USER %>!',
+}
+def fake_read_file(filename='-'):
+    return files[filename]
+
+@pytest.mark.parametrize('argv, env, expected', [
+    ([], {},
+        ('A simple STDIN example\n', '') ),
+
+    (['hello.txt', '--USER=Alice'], {},
+        ('Hello, Alice!\n', '') ),
+    (['hello.txt'], {'USER': 'Bob'},
+        ('Hello, Bob!\n', '') ),
+    (['hello.txt'], {},
+        ('', 'Missing parameter: USER\n') ),
+])
+@patch('formant.cli.read_file', fake_read_file)
+def test_fmnt_print(argv, env, expected, capsys):
+    fmnt_print(*argv, **env)
+    assert capsys.readouterr() == expected
